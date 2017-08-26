028ba861763d9ee8bf4b5c6721f67b47957e7bbe
liyuank <spirit84@gmail.com>
2017-08-26 01:07:16 -0700

    Create epur_str.c

---
+++ epur_str.c
@@ -0,0 +1,10 @@
+/*
+**  Write a program that takes a string and displays the words
+**	separated by a single space.
+**	The last word will be followed by a '\n' (even if there is none).
+**	There will be no spaces before the first or after the last word.
+**	A word is a string separated by either spaces or tabulations,
+**	or the start or the end of the string.
+
+**	If there are no parameters, epur_str displays '\n'.
+*/
