1a5d316253af2bbddba9fb96df40aabf5cea9c7b
liyuank <spirit84@gmail.com>
2017-08-26 01:10:11 -0700

    Create rotone.c

---
+++ rotone.c
@@ -0,0 +1,14 @@
+/*
+
+	Write a program that takes a string and displays it after
+	replacing each character by the next one in alphabetical order.
+
+	'z' becomes 'a' and 'Z' becomes 'A'.
+
+	Upper case letters stay upper case. Lower case letter stay lower case.
+
+	The output will always be followed by a '\n'.
+	If no parameters are passed, the program displays '\n'.
+  
+  */
+  
