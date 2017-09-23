daf6c8bd2b66fab19d750c43efe536df09361f92
liyuank <12709879+liyuank@users.noreply.github.com>
2017-09-22 23:08:44 -0700

    Update fprime.c

--- exam/fprime.c
+++ exam/fprime.c
@@ -6,13 +6,34 @@
 **  fprime.c
 **  display all the prime factors of the in put number
 **  ex: 2*2*3 (12)
-**  Aloowed function: write
+**  Aloowed function: write, printf
 */
 #include <unistd.h>
 
 void  ft_putchar(char c)
 {
   write(1, &c, 1);
+}
+
+int   prime_factor(int nb)
+{
+  int i;
+  
+  i = 2;
+  while (i < nb)
+  {
+    if ( nb % i == 0)
+    {
+      printf("%d*", i);
+      nb /= i;
+    }
+    else
+    {
+      i++;
+    }
+    printf("%d", nb);
+  }
+  return (nb);
 }
 
 int   main(int argc, char **argv)
