50159c65808acede1fb3c719094c68498393ff7c
liyuank <12709879+liyuank@users.noreply.github.com>
2017-11-01 17:54:04 -0700

    Update ft_strcmp.c

--- exam/ft_strcmp.c
+++ exam/ft_strcmp.c
@@ -11,3 +11,27 @@
 **
 **
 */
+#include <stdio.h>
+
+int     ft_strcmp(char *s1, char *s2)
+{
+    while (*s1 == *s2)
+    {
+        if (*s1 == '\0')
+        {
+            return (0);
+        }
+        s1++;
+        s2++;
+    }
+    return ((unsigned char)*s1 - (unsigned char)*s2);
+}
+// test the ft_strcmp function:
+int main(void) {
+	// your code goes here
+	char *ss = "bbd";
+	char *dd = "abd";
+	
+	printf("%d", ft_strcmp(ss, dd));
+	return 0;
+}
